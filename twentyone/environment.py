fcf185b6e201d67036975d657f6d12ceae1e34e4
Matthew G <mattbg29@gmail.com>
2024-03-18 14:17:14 -0400

    updates to the environment to include 6 decks, and removal of some extraneous information

--- twentyone/environment.py
+++ twentyone/environment.py
@@ -51,8 +51,9 @@ class Blackjack:
         u_idx = 100 * self.usable_ace
         return a_idx + d_idx + u_idx
 
-    def get_next_state(self):
+    def get_next_state(self, open_cards):
         new_card = self.deck.deal_card()
+        open_cards.append(new_card)
         self.agent_total += new_card
         if self.agent_total > 21 and self.usable_ace == 1:
             self.usable_ace = 0
@@ -61,7 +62,7 @@ class Blackjack:
             new_state = 201      # 201 is the losing state
         else:
             new_state = self.get_state_index()
-        return new_state
+        return new_state, open_cards
 
     def reset(self):
         self.agent_total = 0
@@ -82,6 +83,7 @@ class Blackjack:
         # deal two cards to the agent
         card_1 = self.deck.deal_card()
         card_2 = self.deck.deal_card()
+        open_cards = [d_card_2, card_1, card_2]
         self.agent_total = card_1 + card_2
         if card_1 == 1 or card_2 == 1:
             self.usable_ace = 1
@@ -98,6 +100,7 @@ class Blackjack:
         else:
             while self.agent_total < 12:
                 new_card = self.deck.deal_card()
+                open_cards.append(new_card)
                 self.agent_total += new_card
                 if new_card == 1 and self.usable_ace == 0 and self.agent_total < 12:
                     self.usable_ace = 1
@@ -107,19 +110,20 @@ class Blackjack:
             self.current_state = self.get_state_index()
 
         # reset complete; return the initial state
-        return self.current_state
+        return self.current_state, open_cards
 
     # Use the agent's action to determine the next state and reward
     def execute_action(self, action):
-        game_end = False
         new_state = -1
         reward = math.inf
+        open_cards = []
 
         # action is 'stick'
         if action == 0:
             # dealer's turn
             while self.dealer_total < 17:
                 new_card = self.deck.deal_card()
+                open_cards.append(new_card)
                 self.dealer_total += new_card
                 if new_card == 1 and self.dealer_ace == 0 and self.dealer_total < 12:
                     self.dealer_ace = 1
@@ -131,35 +135,29 @@ class Blackjack:
                 # dealer busted; agent wins
                 new_state = 203
                 reward = 1
-                game_end = True
             else:
                 if self.dealer_total > self.agent_total:
                     # dealer wins
                     new_state = 201
                     reward = -1
-                    game_end = True
                 elif self.dealer_total < self.agent_total:
                     # agent wins
                     new_state = 203
                     reward = 1
-                    game_end = True
                 else:
                     # tie
                     new_state = 202
                     reward = 0
-                    game_end = True
 
         # action is 'hit'
         elif action == 1:
-            new_state = self.get_next_state()
+            new_state, open_cards = self.get_next_state(open_cards)
             if new_state == 201:
                 reward = -1
-                game_end = True
             else:
                 reward = 0
-                game_end = False
 
         self.current_state = new_state
-        return new_state, reward, game_end
+        return new_state, reward, open_cards
 
 
